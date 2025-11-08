84db063308c0016a9b421da3b49acdd73ff11571
luminous-sky <241473537+luminous-sky@users.noreply.github.com>
2025-11-08 14:38:02 -0800

    Load start position from save file
    Place the player based on the saved position in the save data.
    
    The starting location on the map is loaded from MAP_POSITION_KEY in the save data. If this key does not exist, then the location at the house is used.
    
    Reset the current save file to a blank dictionary when a new game is started. This resets the starting location to the house.

--- src/scripts/menu_buttons/load_game_label.gd
+++ src/scripts/menu_buttons/load_game_label.gd
@@ -1,10 +1,14 @@
 extends RichTextLabel
 
+const MAIN_SCENE = preload("uid://kpetl5ut10yo")
+
 const UNAVAILABLE_COLOR := "#a0a0a0"
 
 var _has_data := false
+var _started_interact := false
 
 @onready var data_manager: DataManager = %DataManager
+@onready var transition_screen: TransitionScreen = $"../../../../../TransitionScreen"
 
 
 # Check to see if any existing data already exists
@@ -18,5 +22,12 @@ func _ready() -> void:
 func button_press() -> void:
 	if _has_data:
 		print("Loading data!")
+		_started_interact = true
+		transition_screen.fade_in()
 	else:
 		print("No data!")
+
+
+func _on_transition_screen_transition_ended(animation_name: StringName) -> void:
+	if _started_interact and animation_name == TransitionScreen.FADE_IN_NAME:
+		get_tree().change_scene_to_packed(MAIN_SCENE)
--- src/scripts/menu_buttons/new_game_label.gd
+++ src/scripts/menu_buttons/new_game_label.gd
@@ -3,6 +3,7 @@ extends RichTextLabel
 const MAIN_SCENE = preload("uid://kpetl5ut10yo")
 
 var _has_data := false
+var _started_interact := false
 
 @onready var data_manager: DataManager = %DataManager
 @onready var transition_screen: TransitionScreen = $"../../../../../TransitionScreen"
@@ -15,9 +16,15 @@ func _ready() -> void:
 
 func button_press() -> void:
 	print("Start!")
+	
+	# Save old data to a backup file, and delete existing data
+	data_manager.save_data({}, true)
+	
+	# Transition into the game
+	_started_interact = true
 	transition_screen.fade_in()
 
 
 func _on_transition_screen_transition_ended(animation_name: StringName) -> void:
-	if animation_name == TransitionScreen.FADE_IN_NAME:
+	if _started_interact and animation_name == TransitionScreen.FADE_IN_NAME:
 		get_tree().change_scene_to_packed(MAIN_SCENE)
